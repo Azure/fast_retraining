06bcb6db78bdf12eefd778f3ac8fbc3f8356e683
mathew salvaris <masalvar@microsoft.com>
2017-03-28 12:09:19 +0100

    Adds the environment scripts

---
+++ environment/activate_env_vars.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# The location of the repository on the host machine
+REPOSPATH=$HOME'/repos/'
+export PROJECTPATH=$REPOSPATH'strata'
+
+
+# Add custom libraries to the python path
+export OLD_PYTHON_PATH=$PYTHONPATH
+export PYTHONPATH=$PYTHONPATH:$PROJECTPATH'/experiments' # Adds the repository to the python path
+
+# Add scripts to path
+export OLD_PATH=$PATH
+export PATH=$PATH:PROJECTPATH
+
+export MOUNT_POINT=/fileshare               # The mounting location for the data
+export CACHE_DIR=$MOUNT_POINT'/cache'
+export STORAGE_URI = //migonzadldsvm2storage.file.core.windows.net/strata-fileshare
+
+echo Me Gusta!
---
+++ environment/deactivate_env_vars.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+export PYTHONPATH=$OLD_PYTHON_PATH
+export PATH=$OLD_PATH
+export MOUNT_POINT=
+export CACHE_DIR=
+export STORAGE_URI=
+export PROJECTPATH=
+echo Noooooooooooooooo
