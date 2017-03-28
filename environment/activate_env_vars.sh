73d63a466730813b9cf799b0ee7cf6f0c1376dca
mathew salvaris <masalvar@microsoft.com>
2017-03-28 19:22:20 +0100

    Fixes pythonpath

--- environment/activate_env_vars.sh
+++ environment/activate_env_vars.sh
@@ -7,7 +7,7 @@ export PROJECTPATH=$REPOSPATH'fast_retraining'
 
 # Add custom libraries to the python path
 export OLD_PYTHON_PATH=$PYTHONPATH
-export PYTHONPATH=$PYTHONPATH:$PROJECTPATH'/experiments' # Adds the repository to the python path
+export PYTHONPATH=$PYTHONPATH:$PROJECTPATH # Adds the repository to the python path
 
 # Add scripts to path
 export OLD_PATH=$PATH
