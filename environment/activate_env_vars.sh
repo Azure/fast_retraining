2409fcfd303bb046d5f9e5bbf598640868b214e9
Miguel Fierro <miguelgfierro@users.noreply.github.com>
2017-06-20 15:05:55 +0100

    updated documentation

--- environment/activate_env_vars.sh
+++ environment/activate_env_vars.sh
@@ -13,8 +13,6 @@ export PYTHONPATH=$PYTHONPATH:$PROJECTPATH # Adds the repository to the python p
 export OLD_PATH=$PATH
 export PATH=$PATH:PROJECTPATH
 
-export MOUNT_POINT=/fileshare               # The mounting location for the data
-export CACHE_DIR=$MOUNT_POINT'/cache'
-export STORAGE_URI=//migonzadldsvm2storage.file.core.windows.net/strata-fileshare
-
+# The mounting location for the data
+export MOUNT_POINT=/fileshare               
 echo Me Gusta!
--- environment/deactivate_env_vars.sh
+++ environment/deactivate_env_vars.sh
@@ -4,6 +4,4 @@ export PYTHONPATH=$OLD_PYTHON_PATH
 export PATH=$OLD_PATH
 export MOUNT_POINT=
 export CACHE_DIR=
-export STORAGE_URI=
-export PROJECTPATH=
 echo Noooooooooooooooo
